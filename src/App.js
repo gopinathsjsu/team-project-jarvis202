cb2d7031ec649d6c68b125d1dc56bd709bdc1314
srujana-koripalli <koripalli.srujana@gmail.com>
2021-03-16 17:44:35 -0700

    SignIn & SignUp

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import Home from './Home';
-
 import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
-
+import React from 'react';
+import SignIn from './SignIn';
 import './App.css';
+import Amplify, { Auth } from 'aws-amplify';
+import Container from '@material-ui/core/Container';
+import awsconfig from './aws-exports';
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
+import SignUp from './SignUp';
+import ConfirmSignUp from './SignUp/ConfirmSignUp';
+import './App.css';
+
+Amplify.configure(awsconfig);
+
+
 
 const theme = createMuiTheme({
   palette: {
@@ -17,14 +28,27 @@ const theme = createMuiTheme({
 });
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+  const onIsLoggedIn = (isLoggedIn) => {
+    setIsLoggedIn(isLoggedIn);
+  }
+
   return (
-    <div className="BankingApp">
-      <ThemeProvider theme={theme}>
-        <Home />
-      </ThemeProvider>
-      
-    </div>
+    <Router >
+      <div className="BankingApp">
+        <Container maxWidth={false}>
+          <ThemeProvider theme={theme}>
+            <Switch>
+              <Route path='/signIn' exact component={() => <SignIn onIsLoggedIn={onIsLoggedIn} />} />
+              <Route path='/signUp' exact component={SignUp} />
+              <Route path='/confirmSignUp/:email' exact component={ConfirmSignUp} />
+              <Home />
+            </Switch>
+          </ThemeProvider>
+        </Container>
+      </div>
+    </Router >
   );
 }
 
-export default App;
+export default App
